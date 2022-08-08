89daac348b5e759be74ae57aeae212dc6686cd85
stephane <stephanexc@gmail.com>
2022-08-08 15:03:01 -0400

    session1

---
+++ models/game.js
@@ -0,0 +1,23 @@
+class Game {
+
+    constructor(players,deck){
+     this.players = players;
+     this.deck = deck;
+    };
+
+    start(){
+        for(let player in this.players){
+            player.hand
+        }
+    }
+
+}
+
+
+
+
+
+
+
+
+module.exports = Game;
